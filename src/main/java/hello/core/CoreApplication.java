ca609f45cd79ae3820bdd6a59cde2bb2f39a9310
junho1131 <45115780+junho1131@users.noreply.github.com>
2023-05-02 00:45:24 +0900

    study: "김영한 스프링 핵심원리 2일차"

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreApplication {
-	//ㅌ
+	
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
